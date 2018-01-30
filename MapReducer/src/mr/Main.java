dcb6f7c85adab19b04c6bc4b94cc5d83de9e2169
CzarEc <czar.echavez@yahoo.co.uk>
2018-01-30 23:07:56 +0000

    -

---
+++ MapReducer/src/mr/Main.java
@@ -0,0 +1,17 @@
+package mr;
+
+/**
+ * Main class
+ * the entry point of the program which holds the main
+ * @author Czar Ian Echavez
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
